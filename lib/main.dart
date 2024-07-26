7182a5052827ccf7e27c1a531fe647ad205ddb39
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2024-07-26 18:02:42 +0300

    feat: add http-service

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pokemons_app/pages/home_page.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pokemons_app/services/http_service.dart';
 
 void main() async {
+  _setupService();
   runApp(const ProviderScope(child: MyApp()));
+}
+
+Future<void> _setupService() async {
+  GetIt.instance.registerSingleton<HTTPService>(
+    HTTPService(),
+  );
 }
 
 class MyApp extends StatelessWidget {
