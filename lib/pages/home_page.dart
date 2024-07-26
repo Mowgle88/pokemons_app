02e972efaa9be30642aabe562bb0974790ef57e4
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2024-07-26 22:19:44 +0300

    feat: add infinite scrolling

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,12 +18,38 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
+  final ScrollController _allPokemonListScrollController = ScrollController();
+
+  late HomePageController _homePageController;
   late HomePageData _homePageData;
+
+  @override
+  void initState() {
+    super.initState();
+    _allPokemonListScrollController.addListener(_scrollListenr);
+  }
+
+  @override
+  void dispose() {
+    _allPokemonListScrollController.removeListener(_scrollListenr);
+    _allPokemonListScrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListenr() {
+    if (_allPokemonListScrollController.offset >=
+            _allPokemonListScrollController.position.maxScrollExtent * 1 &&
+        !_allPokemonListScrollController.position.outOfRange) {
+      print("Reach end of list");
+      _homePageController.loadData();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
+    _homePageController = ref.watch(homePageControllProvider.notifier);
     _homePageData = ref.watch(homePageControllProvider);
 
     return Scaffold(
@@ -51,6 +77,7 @@ class _HomePageState extends ConsumerState<HomePage> {
                       SizedBox(
                         height: size.height * 0.6,
                         child: ListView.builder(
+                          controller: _allPokemonListScrollController,
                           itemCount: _homePageData.data?.results?.length ?? 0,
                           itemBuilder: (context, index) {
                             PokemonListResult pokemon =
